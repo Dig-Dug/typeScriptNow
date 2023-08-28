183e95cfbcddbdb049a496c900fdc849daad7d5f
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-08-28 21:14:37 +0200

    yay :/

---
+++ app.js
@@ -0,0 +1,9 @@
+var ms = "jeyo lol";
+var ms1 = "kkoikok";
+var ha = document.createElement('h1');
+var h3 = document.createElement('p');
+ha.textContent = ms;
+h3.textContent = ms1;
+document.body.appendChild(ha);
+document.body.appendChild(h3);
+console.log(ms);
---
+++ app.ts
@@ -0,0 +1,9 @@
+let ms: string = "jeyo lol";
+let ms1: string = "kkoikok";
+let ha = document.createElement('h1')
+let h3 = document.createElement('p')
+ha.textContent = ms
+h3.textContent = ms1
+document.body.appendChild(ha)
+document.body.appendChild(h3)
+console.log(ms)
